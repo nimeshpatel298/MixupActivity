e76397e8ace5b32d9d19ac89eaa35cce7d8efdda
nimesh <nimesh.patel298@gmail.com>
2019-05-26 21:03:58 +0530

    newly added file

---
+++ MixupActivity/MixupActivity/Controllers/AccountController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MixupActivity.Controllers
+{
+    using System.Web.Security;
+
+    using MixupActivity.Context;
+    using MixupActivity.Models;
+
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Person model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AppContext context = new AppContext())
+                {
+                    var user = context.Persons.FirstOrDefault(person => person.LoginId.Equals(model.LoginId) && person.Password.Equals(model.Password));
+
+                    if (user != null)
+                    {
+
+                        var person = JsonConvert.SerializeObject(new { PersonGuid = user.PersonGuid, LoginId = user.LoginId, PersonName = user.PersonName, Email = user.Email });
+                        var ticket = new FormsAuthenticationTicket(1, user.PersonName, DateTime.Now, DateTime.Now.AddMinutes(30), true, person, FormsAuthentication.FormsCookiePath);
+                        var hash = FormsAuthentication.Encrypt(ticket);
+                        var ck = new HttpCookie("AuthCookie", hash);
+                        if (ticket.IsPersistent)
+                            ck.Expires = ticket.Expiration;
+
+
+                        Response.Cookies.Add(ck);
+
+                        if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                        {
+                            return Redirect(returnUrl);
+                        }
+
+                        return RedirectToAction("Index", "Transactions");
+
+                    }
+                    ModelState.AddModelError("LoginFailed", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+
+            FormsAuthentication.SignOut();
+
+            HttpCookie cookie1 = new HttpCookie("AuthCookie", "");
+            cookie1.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie1);
+            return RedirectToAction("Login", "Account");
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AppContext context = new AppContext())
+                {
+                    context.Persons.Add(model);
+                    var result = context.SaveChanges();
+                    if (result >= 0)
+                    {
+                        return RedirectToAction("Index", "Transactions");
+                    }
+                    ModelState.AddModelError("RegisterFailed", "Failed to register. Please try again.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+    }
+}
---
+++ MixupActivity/MixupActivity/CustomAuthentication/CustomPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace MixupActivity.CustomAuthentication
+{
+    public class CustomPrincipal : IPrincipal
+    {
+        public string LoginId { get; set; }
+        public Guid PersonGuid { get; set; }
+        public string PersonName { get; set; }
+        public string Email { get; set; }
+
+        public IIdentity Identity
+        {
+            get; private set;
+        }
+
+        public CustomPrincipal(string username)
+        {
+            Identity = new GenericIdentity(username);
+        }
+
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
